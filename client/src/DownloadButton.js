104cbf55eff851636b4ccd093f532692f1b91521
vnp23 <vnp23@imperial.ac.uk>
2024-04-27 17:21:07 +0100

    Report button now works. Need to connect it to server which generates report.

--- client/src/DownloadButton.js
+++ client/src/DownloadButton.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
 class FileDownloadButton extends React.Component {
+
+
+     getReport = async () => {
+        try{
+            const response = await fetch('/getReport', {
+                method: "GET"
+            });
+            console.log("Trying to get report data");
+            if(response.status === 200) {
+                const {reportContent} = await response.json();
+                return reportContent;
+            } else {
+                console.log("Error - API response for getting report - " + response.status);
+            }
+        } catch (e){
+            console.log("Could not get report due to "+ e);
+        }
+        alert("Report could not be generated.");
+    }
+
+
+
     // Function to handle the file download
-    handleDownload = () => {
+    handleDownload = async () => {
         // Define the content of the file
-        const fileContent = 'This is the content of the file.';
+        const fileContent = await this.getReport();
+        if (!fileContent) {
+            return;
+        }
 
         // Create a Blob object with the file content
-        const blob = new Blob([fileContent], { type: 'text/plain' });
+        const blob = new Blob([fileContent], {type: 'text/plain'});
 
         // Create a temporary anchor element
         const anchorElement = document.createElement('a');
